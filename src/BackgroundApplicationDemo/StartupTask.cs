e79d6be5cc116f925709e29d69f9ad8deb70e9af
ToanNM4 <ToanNM4@fsoft.com.vn>
2017-11-01 10:34:56 -0400

    Init source code

---
+++ src/BackgroundApplicationDemo/StartupTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundApplicationDemo
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+        }
+    }
+}
